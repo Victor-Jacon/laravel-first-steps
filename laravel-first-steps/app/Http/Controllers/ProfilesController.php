cde6b77d7d0a96f4a8d458618ce01182b93c4cea
Victor Jacon <74216766+Victor-Jacon@users.noreply.github.com>
2021-10-13 00:34:40 -0300

    receiving a request from the frontend + getting the username from the Db + sending it to the frontend so it can be displayed.
    Changed the route (so it can receive a "user" param. The route profile will call a profilesController. This route will have an alias of "profile.show"
    
    Then we created the ProfilesController
    
    And we showed a dynamic data coming from the Controller, instead of showing a static data.

--- laravel-first-steps/app/Http/Controllers/ProfilesController.php
+++ laravel-first-steps/app/Http/Controllers/ProfilesController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfilesController extends Controller
 {
-    //
+    public function index($user) {
+        $user = User::find($user);
+
+        $sendToView = [
+            'user' => $user,
+        ];
+
+        return view('home', $sendToView);
+    }
 }
